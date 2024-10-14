0f6fc87d2d12c8096cf7a5baf17c62b096393b3c
READMEmaybe <haroune11411@gmail.com>
2024-10-14 20:35:04 +0100

    feat(sockets): implemented socket.io connection, and first iteration of create-game

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import authRouter from './routes/auth.js';
 import cookieParser from 'cookie-parser';
 import { checkAuth } from './middlewares/authMiddleware.js';
 import path from 'path';
+import http from 'http';
+import { Server } from 'socket.io';
+import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,6 +26,32 @@ app.get('/', checkAuth, (req, res) => {
 	}
 });
 
-app.listen(PORT, () => {
+const games = new Map();
+const userSocketMap = new Map();
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
+
+	socket.on('auth', (userId) => {
+		userSocketMap.set(userId, socket.id);
+		socket.userId = userId;
+	});
+
+	socket.on('create-game', () => {
+		if (!socket.userId) {
+			return;
+		}
+		const gameId = uuidv4(); 
+		games.set(gameId, { players: [socket.userId] });
+		socket.join(gameId);
+		socket.emit('game-created', { gameId });
+	});
+});
+
+server.listen(PORT, () => {
 	  console.log(`Server is running on PORT ${PORT}`);
 });
