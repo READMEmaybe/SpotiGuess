4c507528e0b91e2c4aa6718f8573972526400253
READMEmaybe <haroune11411@gmail.com>
2024-10-15 15:26:33 +0100

    refactor: added socketServices

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import dotenv from 'dotenv';
 import authRouter from './routes/auth.js';
 import cookieParser from 'cookie-parser';
 import { checkAuth } from './middlewares/authMiddleware.js';
-import path from 'path';
 import http from 'http';
 import { Server } from 'socket.io';
-import { v4 as uuidv4 } from 'uuid';
+import { handleSocketConnection } from './services/socketServices.js';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 dotenv.config();
 const app = express();
@@ -14,57 +15,22 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = new Server(server);
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// app.use(express.static(join(__dirname, 'public')));
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/auth', authRouter);
 app.get('/', checkAuth, (req, res) => {
 	if (req.isAuthenticated) {
-		res.sendFile(path.join(process.cwd(), 'public', 'logged.html'));
+		res.sendFile(join(__dirname, 'public', 'logged.html'));
 	} else {
-		res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
+		res.sendFile(join(__dirname, 'public', 'index.html'));
 	}
 });
 
-const games = new Map();
-const userSocketMap = new Map();
-
-io.on('connection', (socket) => {
-	console.log('a user connected');
-
-	socket.on('disconnect', () => {
-		console.log('user disconnected');
-	});
-
-	socket.on('auth', (userId) => {
-		userSocketMap.set(userId, socket.id);
-		socket.userId = userId;
-	});
-
-	socket.on('create-game', () => {
-		if (!socket.userId) {
-			return;
-		}
-		const gameId = uuidv4(); 
-		games.set(gameId, { players: [socket.userId] });
-		socket.join(gameId);
-		socket.emit('game-created', { gameId });
-	});
-
-	socket.on('join-game', (gameId) => {
-		if (!socket.userId) {
-			return;
-		}
-		const game = games.get(gameId);
-		if (!game) {
-			socket.emit('game-not-found');
-			return;
-		}
-		game.players.push(socket.userId);
-		socket.join(gameId);
-		io.to(gameId).emit('game-joined', { gameId });
-	});
-});
+handleSocketConnection(io);
 
 server.listen(PORT, () => {
 	  console.log(`Server is running on PORT ${PORT}`);
---
+++ src/services/socketServices.js
@@ -0,0 +1,47 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const games = new Map();
+const userSocketMap = new Map();
+
+function handleSocketConnection(io) {
+	io.on('connection', (socket) => {
+		console.log('a user connected');
+
+		socket.on('disconnect', () => {
+			console.log('user disconnected');
+		});
+
+		socket.on('auth', (userId) => {
+			userSocketMap.set(userId, socket.id);
+			socket.userId = userId;
+		});
+
+		socket.on('create-game', () => {
+			if (!socket.userId) {
+				return;
+			}
+			const gameId = uuidv4(); 
+			games.set(gameId, { players: [socket.userId] });
+			socket.join(gameId);
+			socket.emit('game-created', { gameId });
+		});
+
+		socket.on('join-game', (gameId) => {
+			if (!socket.userId) {
+				return;
+			}
+			const game = games.get(gameId);
+			if (!game) {
+				socket.emit('game-not-found');
+				return;
+			}
+			game.players.push(socket.userId);
+			socket.join(gameId);
+			io.to(gameId).emit('game-joined', { gameId });
+		});
+	});
+}
+
+export {
+	handleSocketConnection,
+};
